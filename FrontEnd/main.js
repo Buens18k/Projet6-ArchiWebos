d949d7e643b512b5cd6e1838bd9569abc42df7bc
Bruno Vang <buens@outlook.fr>
2023-09-27 21:28:06 +0200

    fonction initialisation

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,9 +1,20 @@
+//  fonction de récupération des données API works
 async function fetchWorks() {
   // Récupération des données de l'API
   const reponse = await fetch("http://localhost:5678/api/works");
-  const works = await reponse.json();
+  //  retourne en json
+  return await reponse.json();
+  // Test de fonctionnement
+  //   console.log(works);
+}
 
+// fonction d'initialisation
+async function init() {
+  // récupère les données API stockées works
+  const works = await fetchWorks();
+  // Test de fonctionnement
   console.log(works);
 }
 
-fetchWorks();
+// appel de la fonction d'initialisation
+init();
