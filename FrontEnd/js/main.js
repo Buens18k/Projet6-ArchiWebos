eef42ead8f0b3bd7356bd0d2f1813933bd245da3
Bruno Vang <buens@outlook.fr>
2023-10-02 22:04:12 +0200

    ok homePageEdit

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -100,7 +100,7 @@ function createButtonFilter(categoryFetch) {
     // console.log(categoryFetch);
 
     // appelle de la fonction qui fait disparaitre les boutons lorsque le user est connecter
-    disappearBntFilterDisplay ()
+    disappearBntFilterDisplay();
   });
 }
 
@@ -181,27 +181,61 @@ function checkAuthentification() {
 }
 
 // function check l'autification si oui ont fait disparaitre les btnFilters
-function disappearBntFilterDisplay (){
+function disappearBntFilterDisplay() {
   // récupère le token
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   // récupère la div filter
-  const filterDiv = document.querySelector('.filter');
+  const filterDiv = document.querySelector(".filter");
 
   // vérifie si le user est authentifier
-  if(token){
-    filterDiv.style.display = 'none';
-  }else{
-    filterDiv.style.display = 'flex';
+  if (token) {
+    filterDiv.style.display = "none";
+  } else {
+    filterDiv.style.display = "flex";
   }
+}
 
+// function ajoute le svg + l'ancre modifier
+function addSvgAncre() {
+  // récupère le token
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    // Créer le SVG
+    const editSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    editSvg.setAttribute("width", "16");
+    editSvg.setAttribute("height", "16");
+    editSvg.setAttribute("viewBox", "0 0 16 16");
+    editSvg.setAttribute("fill", "none");
+    editSvg.innerHTML = `
+      <path d="M13.5229 1.68576L13.8939 2.05679C14.1821 2.34503 14.1821 2.81113 13.8939 3.0963L13.0016 3.99169L11.5879 2.57808L12.4803 1.68576C12.7685 1.39751 13.2346 1.39751 13.5198 1.68576H13.5229ZM6.43332 7.73578L10.5484 3.61759L11.9621 5.03121L7.84387 9.14633C7.75494 9.23525 7.64455 9.29964 7.52496 9.33337L5.73111 9.84546L6.2432 8.05162C6.27693 7.93203 6.34133 7.82164 6.43025 7.73271L6.43332 7.73578ZM11.4408 0.646245L5.39074 6.6932C5.12397 6.95998 4.93078 7.28808 4.82959 7.64685L3.9526 10.7133C3.879 10.9708 3.94953 11.2468 4.13965 11.4369C4.32977 11.627 4.60574 11.6976 4.86332 11.624L7.92973 10.747C8.29156 10.6427 8.61967 10.4495 8.88338 10.1858L14.9334 4.13888C15.7951 3.27722 15.7951 1.87894 14.9334 1.01728L14.5624 0.646245C13.7007 -0.215415 12.3024 -0.215415 11.4408 0.646245ZM2.69844 1.84214C1.20816 1.84214 0 3.05031 0 4.54058V12.8812C0 14.3715 1.20816 15.5796 2.69844 15.5796H11.0391C12.5293 15.5796 13.7375 14.3715 13.7375 12.8812V9.44683C13.7375 9.039 13.4094 8.71089 13.0016 8.71089C12.5937 8.71089 12.2656 9.039 12.2656 9.44683V12.8812C12.2656 13.5589 11.7167 14.1078 11.0391 14.1078H2.69844C2.02076 14.1078 1.47188 13.5589 1.47188 12.8812V4.54058C1.47188 3.86291 2.02076 3.31402 2.69844 3.31402H6.13281C6.54065 3.31402 6.86875 2.98591 6.86875 2.57808C6.86875 2.17025 6.54065 1.84214 6.13281 1.84214H2.69844Z" fill="black"/>    
+    `;
+    editSvg.classList.add("edit-svg");
+
+    // Créer l'ancre modifier
+    const editLinkModify = document.createElement("a");
+    editLinkModify.setAttribute("href", "#");
+    editLinkModify.innerText = "modifier";
+    editLinkModify.classList.add("link-modifier");
+
+    // récupère le titre H2 de lasection portfolio
+    const h2Title = document.querySelector("#portfolio h2");
+    h2Title.classList.add("modify-h2");
+    // ajoute a côté du h2
+    h2Title.appendChild(editSvg);
+    h2Title.appendChild(editLinkModify);
+  }
 }
 
 // function qui supprime le token lors de la fermeture de la page
-function logOutListener(){
-  window.addEventListener(`beforeunload`, (event) =>{
+function logOutListener() {
+  window.addEventListener(`beforeunload`, (event) => {
     // supprime le token du local Storage lors de la fermeture de la page
-    localStorage.removeItem('token');
-  })
+    localStorage.removeItem("token");
+  });
 }
 
 // fonction d'initialisation
@@ -227,8 +261,10 @@ async function init() {
   // test après être authentifier
   checkAuthentification();
 
-  // 
-  logOutListener()
+  addSvgAncre();
+
+  // supprime le token du localStorage à la fermeture de la page
+  logOutListener();
 }
 
 // appel de la fonction d'initialisation au chargement de la page
